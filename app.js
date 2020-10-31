7439ba1ab49d99a2c8ab36655bee5a92002f2171
Yuugen64 <Yuugen64@protonmail.com>
2020-10-30 22:05:11 -0500

    [013-0.1.3]:: EventHandler Fail >> Tried to implement an event handler for the pop out or flip functionality that I would like to implement to make the mobile version more viable for larger groups of information. However, for some reason the reference always returns null. Will look into it tomorrow.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("[app.js]: Connected properly!");
+
+//For the flipping animation.
+const roadmap = document.getElementById("roadmap");
+const projects = document.getElementById("projectsCard");
+const challenges = document.getElementById("challenges");
+const contact = document.getElementById("contact");
