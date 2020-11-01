caa82eed06bca65ab27ff636a254459981f79778
Yuugen64 <Yuugen64@protonmail.com>
2020-10-31 20:53:18 -0500

    [005-0.0.5]: Main layout is broken. Text doesn't align properly, flips weren't working, sizing is off, will redo tomorrow.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 console.log("[app.js]: Connected properly!");
 
-//For the flipping animation.
-const roadmap = document.getElementById("roadmap");
-const projects = document.getElementById("projectsCard");
-const challenges = document.getElementById("challenges");
-const contact = document.getElementById("contact");
+
